ecabf47ccb1755d19a95a7f362170cbe08bd2541
Darshan Trivedi <trivedidarshan30@gmail.com>
2023-06-30 08:33:27 +0530

    store video in db

--- src/services/YoutubeService.js
+++ src/services/YoutubeService.js
@@ -11,15 +11,54 @@ const startFetchingVideos = async (keyword) => {
         let endDate = getPreviousDay(startDate);
         id = setInterval(async () => {
             let query = `type=video&order=date&type=video&maxResults=10&q=${keyword}&key=${API_KEY}&publishedAfter=${getFormattedDate(endDate)}&publishedBefore=${getFormattedDate(startDate)}`;
+            console.log(startDate, " ", endDate);
             const response = await axios.get(baseURL + query);
             const { data } = response;
+            storeVideo(data.items);
             startDate = endDate;
             endDate = getPreviousDay(startDate);
-        }, 1000);
+        }, 20000);
     } catch (error) {
         console.log(error);
     }
 };
+
+// to store video in db
+function storeVideo(videos) {
+
+    videos.forEach(video => {
+        const videoData = {
+            etag: video.etag,
+            videoId: video.id.videoId,
+            publishedAt: video.snippet.publishedAt,
+            channelId: video.snippet.channelId,
+            title: video.snippet.title,
+            description: video.snippet.description,
+            thumbnails: {
+                default: {
+                    url: video.snippet.thumbnails.default.url,
+                    width: video.snippet.thumbnails.default.width,
+                    height: video.snippet.thumbnails.default.height
+                },
+                medium: {
+                    url: video.snippet.thumbnails.medium.url,
+                    width: video.snippet.thumbnails.medium.width,
+                    height: video.snippet.thumbnails.medium.height
+                },
+                high: {
+                    url: video.snippet.thumbnails.high.url,
+                    width: video.snippet.thumbnails.high.width,
+                    height: video.snippet.thumbnails.high.height
+                }
+            },
+            channelTitle: video.snippet.channelTitle,
+            publishTime: video.snippet.publishTime,
+        };
+
+    });
+
+}
+
 
 function stopFetchingVideos() {
     setTimeout(() => {
@@ -41,5 +80,8 @@ function getFormattedDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}T00:00:00Z`;
 }
+
+startFetchingVideos("News");
+stopFetchingVideos();
 
 export { startFetchingVideos, stopFetchingVideos };
